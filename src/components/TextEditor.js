b484b26571e9383e36f227675b73ad09bae51e11
saadmanfarhad <saadmanheavy@gmail.com>
2018-07-31 16:15:43 +0600

    Created initial value in TextEditor

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -2,6 +2,27 @@ import React, { Component } from 'react';
 import { Editor } from 'slate-react';
 import { Value } from 'slate';
 
+const initialValue = Value.fromJSON({
+  document: {
+    nodes: [
+      {
+        object: 'block',
+        type: 'paragraph',
+        nodes: [
+          {
+            object: 'text',
+            leaves: [
+              {
+                text: 'My First Paragraph!',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+});
+
 export default class TextEditor extends Component {
   state = {
     value: '',
